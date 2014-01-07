d2c28771237f798f530f016ac7e02513d10ad8f7
Frank Hunleth <fhunleth@troodon-software.com>
2014-01-07 13:28:51 -0500

    Clean up commented out lines
    These are no longer needed anyway.

--- relhelper.sh
+++ relhelper.sh
@@ -10,10 +10,3 @@ else
 	relx --system_libs $NERVES_SDK_SYSROOT/usr/lib/erlang/lib
 fi
 
-# Trim up the generated release
-#find _rel -name "*~" -type f -print0 | xargs -0 rm -f
-#find _rel -name "src" -type d -print0 | xargs -0 rm -fr
-#find _rel -name "c_src" -type d -print0 | xargs -0 rm -fr
-#find _rel -name "include" -type d -print0 | xargs -0 rm -fr
-#find _rel -name "obj" -type d -print0 | xargs -0 rm -fr
-
