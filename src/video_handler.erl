94152b62695491d1fc43fad39f8c923a9f4fad59
Frank Hunleth <fhunleth@troodon-software.com>
2014-03-01 19:27:57 -0500

    Reformat code to be more similar to presentation

--- src/video_handler.erl
+++ src/video_handler.erl
@@ -7,33 +7,39 @@
 init(_Transport, Req, []) ->
 	{ok, Req, undefined}.
 
-mime_header_for_picture(Picture) ->
-    Header = io_lib:format("Content-Type: image/jpeg\r\nContent-Length: ~B\r\n\r\n",
-			   [byte_size(Picture)]),
-    list_to_binary(Header).
+multipart_header(Picture) ->
+    io_lib:format("Content-Type: image/jpeg\r\n"
+		  "Content-Length: ~B\r\n\r\n",
+		  [byte_size(Picture)]).
 
-boundary() ->
-    <<"\r\n--foofoo\r\n">>.
+boundary() -> <<"foofoo">>.
+delimiter() ->
+    Boundary = boundary(),
+    <<"\r\n--", Boundary/binary, "\r\n">>.
+
+send_first_picture(Req) ->
+    Pic = troodon_cam:get_next_picture(),
+    Msg = [delimiter(), multipart_header(Pic), Pic, delimiter()],
+    ok = cowboy_req:chunk(Msg, Req).
 
 send_pictures(Req) ->
-    Picture = troodon_cam:get_next_picture(),
-    Msg = [boundary(), mime_header_for_picture(Picture), Picture, boundary()],
+    Pic = troodon_cam:get_next_picture(),
+    Msg = [multipart_header(Pic), Pic, delimiter()],
     ok = cowboy_req:chunk(Msg, Req),
-    send_remaining_pictures(Req).
-
-send_remaining_pictures(Req) ->
-    Picture = troodon_cam:get_next_picture(),
-    Msg = [mime_header_for_picture(Picture), Picture, boundary()],
-    ok = cowboy_req:chunk(Msg, Req),
-    send_remaining_pictures(Req).
+    send_pictures(Req).
 
 handle(Req, _State) ->
+    Boundary = boundary(),
+    Headers =
+	[{<<"connection">>, <<"close">>},
+	 {<<"Cache-Control">>, <<"no-cache">>},
+	 {<<"MIME-Version">>, <<"1.0">>},
+	 {<<"content-type">>, <<"multipart/x-mixed-replace; ",
+				"boundary=", Boundary/binary>>}],
     {ok, Req2} = cowboy_req:chunked_reply(200,
-					  [{<<"connection">>, <<"close">>},
-					   {<<"Cache-Control">>, <<"no-cache">>},
-					   {<<"MIME-Version">>, <<"1.0">>},
-					   {<<"content-type">>, <<"multipart/x-mixed-replace; boundary=foofoo">>}],
+					  Headers,
 					  Req),
+    send_first_picture(Req2),
     send_pictures(Req2).
 
 terminate(_Reason, _Req, _State) ->
